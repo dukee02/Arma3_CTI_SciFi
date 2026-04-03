fa0ab1d69d98135b0232d98792fe2eb6826cfd96
dukee02 <admin@vio-community.de>
2026-04-03 15:14:50 +0200

    Fixed: missing Show Map Infantry option

--- VIO-BECTI.Map_name/_RSC_Diffs/Vanilla/Game.hpp
+++ VIO-BECTI.Map_name/_RSC_Diffs/Vanilla/Game.hpp
@@ -4,12 +4,12 @@ respawnDelay = 3;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadName = "VIO-BECTI V2.0.6";
+onLoadName = "VIO-BECTI V2.0.7";
 onLoadMissionTime = false;
-overviewText = "VIO-BECTI V2.0.6";
-overviewTextLocked = "VIO-BECTI V2.0.6";
-onLoadMission = "VIO-BECTI V2.0.6_250820";
-briefingName = "VIO-BECTI V2.0.6";
+overviewText = "VIO-BECTI V2.0.7";
+overviewTextLocked = "VIO-BECTI V2.0.7";
+onLoadMission = "VIO-BECTI V2.0.7_260403";
+briefingName = "VIO-BECTI V2.0.7";
 
 //--- ArmA 3 Specifics
 dev = "dukee";
--- VIO-BECTI.Map_name/_RSC_Diffs/WW2/Game.hpp
+++ VIO-BECTI.Map_name/_RSC_Diffs/WW2/Game.hpp
@@ -4,12 +4,12 @@ respawnDelay = 3;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadName = "VIO-BECTI-WW2 V2.0.6";
+onLoadName = "VIO-BECTI-WW2 V2.0.7";
 onLoadMissionTime = false;
-overviewText="VIO-BECTI-WW2 V2.0.6";
-overviewTextLocked="VIO-BECTI-WW2 V2.0.6";
-onLoadMission = "VIO-BECTI-WW2 V2.0.6_250820";
-briefingName = "VIO-BECTI-WW2 V2.0.6";
+overviewText="VIO-BECTI-WW2 V2.0.7";
+overviewTextLocked="VIO-BECTI-WW2 V2.0.7";
+onLoadMission = "VIO-BECTI-WW2 V2.0.7_260403";
+briefingName = "VIO-BECTI-WW2 V2.0.7";
 
 //--- ArmA 3 Specifics
 dev = "Benny, dukee";
